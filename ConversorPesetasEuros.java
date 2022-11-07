a9642b8e5e2061bb57737f246aae5710a8cbc71e
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-07 16:23:04 +0100

    Add files via upload

---
+++ ConversorPesetasEuros.java
@@ -0,0 +1,14 @@
+/*Programa diseñado por Lucas Bermudo que tiene como objetivo convertir pesetas a euros.*/
+
+public class ConversorPesetasEuros{
+  public static void main(String args[]){
+
+		double cantPesetas=1255881.52;
+		double cambioPesetasEuros=1/166.386;
+		double cantEuros=cantPesetas*cambioPesetasEuros;
+		
+		System.out.printf("El cambio de %.2f", cantPesetas);
+		System.out.printf(" pesetas a euros es: %.2f", cantEuros);
+		System.out.println(" euros");
+  }
+}
