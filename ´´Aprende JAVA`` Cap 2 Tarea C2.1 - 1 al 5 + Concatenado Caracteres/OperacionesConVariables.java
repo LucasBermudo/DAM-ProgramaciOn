b6c6c6a92f48c15c7ef1cc4516fa8fc298eb4e50
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:12:57 +0100

    Update OperacionesConVariables.java

--- ´´Aprende JAVA`` Cap 2 Tarea C2.1 - 1 al 5 + Concatenado Caracteres/OperacionesConVariables.java
+++ ´´Aprende JAVA`` Cap 2 Tarea C2.1 - 1 al 5 + Concatenado Caracteres/OperacionesConVariables.java
@@ -5,23 +5,23 @@
  */
 
 public class OperacionesConVariables{
-  public static void main(String args[]){
-    
-    double x=144;
-    double y=999;
-    double sumaxy = x+y;
-    double restaxy = x-y;
-    double multiplicaciónxy = x*y;
-    double divisiónxy = x/y;
-    
-    System.out.println("El valor de ´´x`` es : " + x);
-    System.out.println("El valor de ´´y`` es : " + y);
-    System.out.printf("La suma de los valores ´´x`` e ´´y`` es = %.3f", sumaxy);
-    System.out.println("");
-    System.out.printf("La resta de los valores ´´x`` e ´´y`` es = %.3f", restaxy);
-    System.out.println("");
-    System.out.printf("La multiplicación de los valores ´´x`` e ´´y`` es = %.3f", multiplicaciónxy);
-    System.out.println("");
-    System.out.printf("La división de los valores ´´x`` e ´´y`` es = %.3f", divisiónxy);
-  }
+	public static void main(String args[]){
+	
+		double x=144;
+		double y=999;
+		double sumaxy = x+y;
+		double restaxy = x-y;
+		double multiplicaciónxy = x*y;
+		double divisiónxy = x/y;
+		
+		System.out.println("El valor de ´´x`` es : " + x);
+		System.out.println("El valor de ´´y`` es : " + y);
+		System.out.printf("La suma de los valores ´´x`` e ´´y`` es = %.3f", sumaxy);
+		System.out.println("");
+		System.out.printf("La resta de los valores ´´x`` e ´´y`` es = %.3f", restaxy);
+		System.out.println("");
+		System.out.printf("La multiplicación de los valores ´´x`` e ´´y`` es = %.3f", multiplicaciónxy);
+		System.out.println("");
+		System.out.printf("La división de los valores ´´x`` e ´´y`` es = %.3f", divisiónxy);
+	}
 }
