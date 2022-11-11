7dc55ef7ca266f766b53dbb5c2f80d9ac2ea9180
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 02:47:10 +0100

    Update Traducción.java

--- ´´Aprende JAVA`` Cap 1 Tarea C1.1 - 1 al 3 + Ejemplo Tabla Colores/Traducción.java
+++ ´´Aprende JAVA`` Cap 1 Tarea C1.1 - 1 al 3 + Ejemplo Tabla Colores/Traducción.java
@@ -6,17 +6,17 @@
  */
 
 public class Traducción{
-  public static void main(String args[]){
-
-    String pal1ing="computer", pal1esp="ordenador", pal2ing="student", pal2esp="alumno/a", pal3ing="cat", pal3esp="gato",
-    pal4ing="penguin", pal4esp="pingüino", pal5ing="machine", pal5esp="máquina", pal6ing="nature", pal6esp="naturaleza",
-    pal7ing="light", pal7esp="luz", pal8ing="green", pal8esp="verde", pal9ing="book", pal9esp="libro",
-    pal10ing="pyramid", pal10esp="pirámide";
-    
-    System.out.println(pal1ing + "	" + pal1esp);
-    System.out.println(pal2ing + "		" + pal2esp);
-    System.out.println(pal3ing + "		" + pal3esp);
-    System.out.println(pal4ing + "		" + pal4esp);
-    System.out.println(pal5ing + "		" + pal5esp);
-  }
+	public static void main(String args[]){
+	
+		String pal1ing="computer", pal1esp="ordenador", pal2ing="student", pal2esp="alumno/a", pal3ing="cat", pal3esp="gato",
+		pal4ing="penguin", pal4esp="pingüino", pal5ing="machine", pal5esp="máquina", pal6ing="nature", pal6esp="naturaleza",
+		pal7ing="light", pal7esp="luz", pal8ing="green", pal8esp="verde", pal9ing="book", pal9esp="libro",
+		pal10ing="pyramid", pal10esp="pirámide";
+		
+		System.out.println(pal1ing + "	" + pal1esp);
+		System.out.println(pal2ing + "		" + pal2esp);
+		System.out.println(pal3ing + "		" + pal3esp);
+		System.out.println(pal4ing + "		" + pal4esp);
+		System.out.println(pal5ing + "		" + pal5esp);
+	}
 }
