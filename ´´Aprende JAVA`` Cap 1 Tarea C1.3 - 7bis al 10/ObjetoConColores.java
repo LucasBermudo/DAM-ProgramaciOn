c612aff992163715edc6588ff0ee8ba87dd82ad2
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 02:59:08 +0100

    Update ObjetoConColores.java

--- ´´Aprende JAVA`` Cap 1 Tarea C1.3 - 7bis al 10/ObjetoConColores.java
+++ ´´Aprende JAVA`` Cap 1 Tarea C1.3 - 7bis al 10/ObjetoConColores.java
@@ -5,20 +5,20 @@
  */
 
 public class ObjetoConColores{
-  public static void main(String args[]){
-    
-    String colorDefecto="\033[39;49m", amarillo="\033[33m", negroClaro="\033[90m", azul="\033[34m", rojo="\033[31m", verde="\033[32m";
-    char square = (char)9632, barraInvertida = (char)92;
-
-    System.out.println(rojo+"                 "+square+square+square+square+square+square+square+square+square+square+square+square+square+square+square+colorDefecto);
-    System.out.println(azul+"                //"+amarillo+"    @"+rojo+" │ "+amarillo+"   @ "+azul+"││");
-    System.out.println(azul+"               //"+verde+"─)"+amarillo+"  ─│"+rojo+" │ "+amarillo+"   │ "+azul+"││");
-    System.out.println(rojo+"         "+square+square+square+square+square+square+square+"───────────────"+square+square+square+square+square+square+square+square+square);
-    System.out.println("        /    ──── "+rojo+"│ "+verde+"  ¬ "+rojo+"│  "+verde+" ¬ "+rojo+"│  ────   │");
-    System.out.println("       │    / "+negroClaro+"── "+rojo+barraInvertida+"│     │     │ / "+negroClaro+"── "+rojo+barraInvertida+"  │");
-    System.out.println("        "+square+square+square+square+negroClaro+" /  "+barraInvertida+rojo+" "+square+square+square+square+square+square+square+square+square+square+square+square+square+square+negroClaro+" /  "+barraInvertida+" "+rojo+square+square);
-    System.out.println(negroClaro+"             "+barraInvertida+"  /                "+barraInvertida+"  /");
-    System.out.println("              ──                  ──");
-    System.out.println(negroClaro+"────────────────────────────────────────────────────"+colorDefecto);
-  }
+	public static void main(String args[]){
+	
+		String colorDefecto="\033[39;49m", amarillo="\033[33m", negroClaro="\033[90m", azul="\033[34m", rojo="\033[31m", verde="\033[32m";
+		char square = (char)9632, barraInvertida = (char)92;
+		
+		System.out.println(rojo+"                 "+square+square+square+square+square+square+square+square+square+square+square+square+square+square+square+colorDefecto);
+		System.out.println(azul+"                //"+amarillo+"    @"+rojo+" │ "+amarillo+"   @ "+azul+"││");
+		System.out.println(azul+"               //"+verde+"─)"+amarillo+"  ─│"+rojo+" │ "+amarillo+"   │ "+azul+"││");
+		System.out.println(rojo+"         "+square+square+square+square+square+square+square+"───────────────"+square+square+square+square+square+square+square+square+square);
+		System.out.println("        /    ──── "+rojo+"│ "+verde+"  ¬ "+rojo+"│  "+verde+" ¬ "+rojo+"│  ────   │");
+		System.out.println("       │    / "+negroClaro+"── "+rojo+barraInvertida+"│     │     │ / "+negroClaro+"── "+rojo+barraInvertida+"  │");
+		System.out.println("        "+square+square+square+square+negroClaro+" /  "+barraInvertida+rojo+" "+square+square+square+square+square+square+square+square+square+square+square+square+square+square+negroClaro+" /  "+barraInvertida+" "+rojo+square+square);
+		System.out.println(negroClaro+"             "+barraInvertida+"  /                "+barraInvertida+"  /");
+		System.out.println("              ──                  ──");
+		System.out.println(negroClaro+"────────────────────────────────────────────────────"+colorDefecto);
+	}
 }
