8882f2d586d88acd51ca03551028438e4992b77a
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:29:51 +0100

    Update ÁreaRectángulo.java

--- ´´Aprende JAVA`` Cap 3 Tarea C3.2/ÁreaRectángulo.java
+++ ´´Aprende JAVA`` Cap 3 Tarea C3.2/ÁreaRectángulo.java
@@ -5,8 +5,8 @@
  */
 
 public class ÁreaRectángulo{
-  public static void main(String args[]){
-
+	public static void main(String args[]){
+	
 		String línea;
 		
 		System.out.print("Por favor, introduce la base del rectángulo: ");
@@ -20,5 +20,5 @@ public class ÁreaRectángulo{
 		altura = Double.parseDouble(línea);
 		
 		System.out.println("El área de un rectángulo de base " + base + " y de altura " + altura + " unidades es " + (base*altura) + " unidades^2");
-  }
+	}
 }
