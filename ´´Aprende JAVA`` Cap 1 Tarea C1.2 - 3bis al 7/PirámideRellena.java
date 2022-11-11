ff0bc1737387bd32ef8168198eb81ce84053844a
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 02:54:41 +0100

    Update PirámideRellena.java

--- ´´Aprende JAVA`` Cap 1 Tarea C1.2 - 3bis al 7/PirámideRellena.java
+++ ´´Aprende JAVA`` Cap 1 Tarea C1.2 - 3bis al 7/PirámideRellena.java
@@ -5,12 +5,12 @@
  */
 
 public class PirámideRellena{
-  public static void main(String args[]){
-  
-    System.out.println("    *    ");
-    System.out.println("   ***   ");
-    System.out.println("  *****  ");
-    System.out.println(" *******  ");
-    System.out.println("*********");
-  }
+	public static void main(String args[]){
+	
+		System.out.println("    *    ");
+		System.out.println("   ***   ");
+		System.out.println("  *****  ");
+		System.out.println(" *******  ");
+		System.out.println("*********");
+	}
 }
