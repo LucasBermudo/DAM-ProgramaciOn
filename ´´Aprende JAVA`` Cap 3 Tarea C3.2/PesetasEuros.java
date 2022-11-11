15616172e285c1a903543a3171e6da8d9c86408e
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:28:50 +0100

    Update PesetasEuros.java

--- ´´Aprende JAVA`` Cap 3 Tarea C3.2/PesetasEuros.java
+++ ´´Aprende JAVA`` Cap 3 Tarea C3.2/PesetasEuros.java
@@ -5,20 +5,20 @@
  */
 
 public class PesetasEuros{
-  public static void main(String args[]){
-
+	public static void main(String args[]){
+	
 		String línea;
-
+		
 		System.out.print("Por favor, introduce la cantidad en pesetas que desees convertir a euros: ");
 		línea = System.console().readLine();
 		double cantPesetas;
 		cantPesetas = Double.parseDouble(línea);
-
+		
 		double cambioPesetasEuros=1/166.386;
 		double cantEuros=cantPesetas*cambioPesetasEuros;
-
+		
 		System.out.printf("El cambio de %.2f", cantPesetas);
 		System.out.printf(" pesetas a euros es: %.2f", cantEuros);
 		System.out.println(" euros.");
-  }
+	}
 }
