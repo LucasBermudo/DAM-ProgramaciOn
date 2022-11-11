bc5a3674328da128856e8dc9b9516d28dc781f2a
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 02:52:57 +0100

    Update HorarioSinColores.java

--- ´´Aprende JAVA`` Cap 1 Tarea C1.2 - 3bis al 7/HorarioSinColores.java
+++ ´´Aprende JAVA`` Cap 1 Tarea C1.2 - 3bis al 7/HorarioSinColores.java
@@ -5,24 +5,24 @@
  */
 
 public class HorarioSinColores{
-  public static void main(String args[]){
-  
-    System.out.println("┌───────────┬──────────────┬──────────────┬─────────────┬──────────────┬──────────────┐");
-    System.out.println("│           │    LUNES     │    MARTES    │  MIÉRCOLES  │    JUEVES    │    VIERNES   │");
-    System.out.println("├───────────┼──────────────┼──────────────┼─────────────┼──────────────┼──────────────┤");
-    System.out.println("│15:15-16:15│  Sist. Inf.  │              │  Sist. Inf. │              │              │");
-    System.out.println("├───────────┼──────────────┤ Programación ├─────────────┤ Programación │ Programación │");
-    System.out.println("│16:15-17:15│              │              │             │              │              │");
-    System.out.println("├───────────┤ Programación ├──────────────┤     FOL     ├──────────────┼──────────────┤");
-    System.out.println("│17:15-18:15│              │ Leng. Marca  │             │    BB.DD.    │    BB.DD.    │");
-    System.out.println("├───────────┼──────────────┴──────────────┴─────────────┴──────────────┴──────────────┤");
-    System.out.println("│18:15-18:30│			        	RECREO				      │");
-    System.out.println("├───────────┼──────────────┬──────────────┬─────────────┬──────────────┬──────────────┤");
-    System.out.println("│18:30-19:30│     FOL      │ Leng. Marca  │             │    BB.DD.    │    BB.DD.    │");
-    System.out.println("├───────────┼──────────────┼──────────────┤  Entornos   ├──────────────┼──────────────┤");
-    System.out.println("│19:30-20:30│              │              │     de      │              │              │");
-    System.out.println("├───────────┤    BB.DD.    │  Sist. Inf.  │  Desarrollo │  Sist. Inf.  │ Leng. Marca  │");
-    System.out.println("│20:30-21:30│              │              │             │              │              │");
-    System.out.println("└───────────┴──────────────┴──────────────┴─────────────┴──────────────┴──────────────┘");
-  }
+	public static void main(String args[]){
+	
+		System.out.println("┌───────────┬──────────────┬──────────────┬─────────────┬──────────────┬──────────────┐");
+		System.out.println("│           │    LUNES     │    MARTES    │  MIÉRCOLES  │    JUEVES    │    VIERNES   │");
+		System.out.println("├───────────┼──────────────┼──────────────┼─────────────┼──────────────┼──────────────┤");
+		System.out.println("│15:15-16:15│  Sist. Inf.  │              │  Sist. Inf. │              │              │");
+		System.out.println("├───────────┼──────────────┤ Programación ├─────────────┤ Programación │ Programación │");
+		System.out.println("│16:15-17:15│              │              │             │              │              │");
+		System.out.println("├───────────┤ Programación ├──────────────┤     FOL     ├──────────────┼──────────────┤");
+		System.out.println("│17:15-18:15│              │ Leng. Marca  │             │    BB.DD.    │    BB.DD.    │");
+		System.out.println("├───────────┼──────────────┴──────────────┴─────────────┴──────────────┴──────────────┤");
+		System.out.println("│18:15-18:30│			        	RECREO				      │");
+		System.out.println("├───────────┼──────────────┬──────────────┬─────────────┬──────────────┬──────────────┤");
+		System.out.println("│18:30-19:30│     FOL      │ Leng. Marca  │             │    BB.DD.    │    BB.DD.    │");
+		System.out.println("├───────────┼──────────────┼──────────────┤  Entornos   ├──────────────┼──────────────┤");
+		System.out.println("│19:30-20:30│              │              │     de      │              │              │");
+		System.out.println("├───────────┤    BB.DD.    │  Sist. Inf.  │  Desarrollo │  Sist. Inf.  │ Leng. Marca  │");
+		System.out.println("│20:30-21:30│              │              │             │              │              │");
+		System.out.println("└───────────┴──────────────┴──────────────┴─────────────┴──────────────┴──────────────┘");
+	}
 }
