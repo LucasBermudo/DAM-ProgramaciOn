7bb2fdac032dcf4cc0b0b1d3706b6e8f4a419899
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 02:42:02 +0100

    Update DatosPersonales.java

--- ´´Aprende JAVA`` Cap 1 Tarea C1.1 - 1 al 3 + Ejemplo Tabla Colores/DatosPersonales.java
+++ ´´Aprende JAVA`` Cap 1 Tarea C1.1 - 1 al 3 + Ejemplo Tabla Colores/DatosPersonales.java
@@ -5,13 +5,13 @@
  */
  
 public class DatosPersonales{
-  public static void main(String args[]){
-  
-    String nombre="Lucas", dirección="Calle Jazmín 4, 2ºB";
-    int teléfono=684289574;
-    
-    System.out.println("El nombre del diseñador de este programa es " + nombre);
-    System.out.println("La dirección de Lucas es " + dirección);
-    System.out.println("El número de teléfono de Lucas es " + teléfono);
-  }
+	public static void main(String args[]){
+	
+		String nombre="Lucas", dirección="Calle Jazmín 4, 2ºB";
+		int teléfono=684289574;
+		
+		System.out.println("El nombre del diseñador de este programa es " + nombre);
+		System.out.println("La dirección de Lucas es " + dirección);
+		System.out.println("El número de teléfono de Lucas es " + teléfono);
+	}
 }
