851dd8d4aa191122dd9537fb11299b7c4efffdd6
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:06:41 +0100

    Update ComportamientoCurioso.java

--- ´´Aprende JAVA`` Cap 2 Tarea C2.1 - 1 al 5 + Concatenado Caracteres/ComportamientoCurioso.java
+++ ´´Aprende JAVA`` Cap 2 Tarea C2.1 - 1 al 5 + Concatenado Caracteres/ComportamientoCurioso.java
@@ -5,14 +5,14 @@
  */
 
 public class ComportamientoCurioso{
-  public static void main(String args[]){
-    
-    char letra1='a';
-    char letra2='b';
-    
-    System.out.println(letra1);
-    System.out.println(letra2);
-    System.out.println(letra1+letra2);
-    System.out.println(letra1+""+letra2);
-  }
+	public static void main(String args[]){
+	
+		char letra1='a';
+		char letra2='b';
+		
+		System.out.println(letra1);
+		System.out.println(letra2);
+		System.out.println(letra1+letra2);
+		System.out.println(letra1+""+letra2);
+	}
 }
