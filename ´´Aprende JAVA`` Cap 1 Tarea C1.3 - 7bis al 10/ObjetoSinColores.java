54d865899f6588315a007b9d3bcea05b40738e13
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:00:55 +0100

    Update ObjetoSinColores.java

--- ´´Aprende JAVA`` Cap 1 Tarea C1.3 - 7bis al 10/ObjetoSinColores.java
+++ ´´Aprende JAVA`` Cap 1 Tarea C1.3 - 7bis al 10/ObjetoSinColores.java
@@ -5,19 +5,19 @@
  */
 
 public class ObjetoSinColores{
-  public static void main(String args[]){
-    
-    char square = (char)9632, barraInvertida = (char)92;
-
-    System.out.println("                 "+square+square+square+square+square+square+square+square+square+square+square+square+square+square+square);
-    System.out.println("                //    @ │    @ ││");
-    System.out.println("               //─)  ─│ │    │ ││");
-    System.out.println("         "+square+square+square+square+square+square+square+"───────────────"+square+square+square+square+square+square+square+square+square);
-    System.out.println("        /    ──── │   ¬ │   ¬ │  ────   │");
-    System.out.println("       │    / ── "+barraInvertida + "│     │     │ / ── "+barraInvertida+"  │");
-    System.out.println("        "+square+square+square+square + " /  "+barraInvertida+" "+square+square+square+square+square+square+square+square+square+square+square+square+square+square+" /  "+barraInvertida+" "+square+square);
-    System.out.println("             "+barraInvertida+"  /                "+barraInvertida+"  /");
-    System.out.println("              ──                  ──");
-    System.out.println("────────────────────────────────────────────────────");
-  }
+	public static void main(String args[]){
+	
+		char square = (char)9632, barraInvertida = (char)92;
+		
+		System.out.println("                 "+square+square+square+square+square+square+square+square+square+square+square+square+square+square+square);
+		System.out.println("                //    @ │    @ ││");
+		System.out.println("               //─)  ─│ │    │ ││");
+		System.out.println("         "+square+square+square+square+square+square+square+"───────────────"+square+square+square+square+square+square+square+square+square);
+		System.out.println("        /    ──── │   ¬ │   ¬ │  ────   │");
+		System.out.println("       │    / ── "+barraInvertida + "│     │     │ / ── "+barraInvertida+"  │");
+		System.out.println("        "+square+square+square+square + " /  "+barraInvertida+" "+square+square+square+square+square+square+square+square+square+square+square+square+square+square+" /  "+barraInvertida+" "+square+square);
+		System.out.println("             "+barraInvertida+"  /                "+barraInvertida+"  /");
+		System.out.println("              ──                  ──");
+		System.out.println("────────────────────────────────────────────────────");
+	}
 }
