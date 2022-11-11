66cc3d0778d308affae0b0f1e62c7ff0ee1f2417
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:36:32 +0100

    Update CharString.java

--- Lucas Bermudo - Programación - Tarea C2.2 Códigos/CharString.java
+++ Lucas Bermudo - Programación - Tarea C2.2 Códigos/CharString.java
@@ -4,12 +4,12 @@
 public class CharString{
 	public static void main(String args[]){
 	
-	char pMayus = 'P', r = 'r', o = 'o', g = 'g', a = 'a', m = 'm', c = 'c', i = 'i', ó = 'ó', n = 'n';
-	String fp = "DAM";
-	
-	System.out.println("Estamos ahora mismo cursando la asignatura de " + pMayus + r + o + g + r + a + m + a + c + i + ó + n + " de 1º de " + fp);
-	System.out.println("");
-	System.out.print("Como acabamos de comprobar, sí se puede mostrar por pantalla en la misma línea y con una misma sentencia de Java"); 
-	System.out.print(" todos las variables de tipo char y de tipo String que hayamos declarado");
+		char pMayus = 'P', r = 'r', o = 'o', g = 'g', a = 'a', m = 'm', c = 'c', i = 'i', ó = 'ó', n = 'n';
+		String fp = "DAM";
+		
+		System.out.println("Estamos ahora mismo cursando la asignatura de " + pMayus + r + o + g + r + a + m + a + c + i + ó + n + " de 1º de " + fp);
+		System.out.println("");
+		System.out.print("Como acabamos de comprobar, sí se puede mostrar por pantalla en la misma línea y con una misma sentencia de Java"); 
+		System.out.print(" todos las variables de tipo char y de tipo String que hayamos declarado");
 	}
 }
