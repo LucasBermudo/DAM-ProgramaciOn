67bb42a04ce6ae405b4d73ff6e1eb5254ee39add
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:23:41 +0100

    Update LecturaDeTexto.java

--- ´´Aprende JAVA`` Cap 3 Tarea C3.1 - 1 al 3 + Lect. Texto + Lects. Núms/LecturaDeTexto.java
+++ ´´Aprende JAVA`` Cap 3 Tarea C3.1 - 1 al 3 + Lect. Texto + Lects. Núms/LecturaDeTexto.java
@@ -5,8 +5,8 @@
  */
 
 public class LecturaDeTexto{
-  public static void main(String args[]){
-
+	public static void main(String args[]){
+	
 		String nombre, apellidos;
 		
 		System.out.print("Por favor, dime cuál es tu nombre: ");
@@ -14,5 +14,5 @@ public class LecturaDeTexto{
 		System.out.print("Por favor, dime cuáles son tus apellidos: ");
 		apellidos = System.console().readLine();
 		System.out.println("Hola " + nombre + " " + apellidos + ", ¡encantado de conocerte!");
- }
+	}
 }
