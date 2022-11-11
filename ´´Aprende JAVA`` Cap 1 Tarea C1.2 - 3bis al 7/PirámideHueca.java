c188336812f6334d36123c32e6cc0da3f98a415c
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 02:53:52 +0100

    Update PirámideHueca.java

--- ´´Aprende JAVA`` Cap 1 Tarea C1.2 - 3bis al 7/PirámideHueca.java
+++ ´´Aprende JAVA`` Cap 1 Tarea C1.2 - 3bis al 7/PirámideHueca.java
@@ -5,12 +5,12 @@
  */
 
 public class PirámideHueca{
-  public static void main(String args[]){
-  
-    System.out.println("    *    ");
-    System.out.println("   * *   ");
-    System.out.println("  *   *  ");
-    System.out.println(" *     *  ");
-    System.out.println("*********");
-  }
+	public static void main(String args[]){
+	
+		System.out.println("    *    ");
+		System.out.println("   * *   ");
+		System.out.println("  *   *  ");
+		System.out.println(" *     *  ");
+		System.out.println("*********");
+	}
 }
