86bae80417d63183ce242560c1c784fa51406148
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:38:10 +0100

    Update IVA.java

--- Lucas Bermudo - Programación - Tarea C2.2 Códigos/IVA.java
+++ Lucas Bermudo - Programación - Tarea C2.2 Códigos/IVA.java
@@ -1,12 +1,16 @@
-/*Programa diseñado por Lucas Bermudo que tiene como objetivo el total de una factura a partir de la base imponible (precio sin IVA).*/
+/**
+ * Calcular el total de una factura a partir de la base imponible (precio sin IVA).
+ * 
+ * @autor Lucas Bermudo Junco
+ */
 
 public class IVA{
 	public static void main(String args[]){
 	
-	double baseImponible = 83.60;
-	double iva = 0.21;
-	double precioFinal = baseImponible * (1+iva);
-	
-	System.out.println("El precio final del artículo es " + (int) precioFinal + " euros.");
+		double baseImponible = 83.60;
+		double iva = 0.21;
+		double precioFinal = baseImponible * (1+iva);
+		
+		System.out.println("El precio final del artículo es " + (int) precioFinal + " euros.");
 	}
 }
