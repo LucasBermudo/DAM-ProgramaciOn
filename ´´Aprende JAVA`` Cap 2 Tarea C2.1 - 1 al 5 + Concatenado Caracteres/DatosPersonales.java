93ee7fe208a013d7d1235ec670534c7ff4bacb2b
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:10:55 +0100

    Update DatosPersonales.java

--- ´´Aprende JAVA`` Cap 2 Tarea C2.1 - 1 al 5 + Concatenado Caracteres/DatosPersonales.java
+++ ´´Aprende JAVA`` Cap 2 Tarea C2.1 - 1 al 5 + Concatenado Caracteres/DatosPersonales.java
@@ -5,12 +5,12 @@
  */
 
 public class DatosPersonales{
-  public static void main(String args[]){
-    
+	public static void main(String args[]){
+	
 		String nombreCompleto="Lucas Bermudo Junco";
 		String dirección="Calle Páramo, 24, 4ºD";
 		int teléfono=648930657;
 		
 		System.out.println("El nombre completo del diseñador de este programa es: " + nombreCompleto + ", su dirección es: " + dirección + " y su teléfono es: " + teléfono + ".");
-  }
+	}
 }
