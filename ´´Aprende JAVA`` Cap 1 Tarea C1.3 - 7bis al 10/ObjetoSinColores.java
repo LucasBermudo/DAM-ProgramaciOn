3997ac2474593493e73889eb9e072301ffdea52f
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 01:59:09 +0100

    Update ObjetoSinColores.java

--- ´´Aprende JAVA`` Cap 1 Tarea C1.3 - 7bis al 10/ObjetoSinColores.java
+++ ´´Aprende JAVA`` Cap 1 Tarea C1.3 - 7bis al 10/ObjetoSinColores.java
@@ -1,9 +1,13 @@
-/*Programa diseñado por Lucas Bermudo que tiene como objetivo pintar por pantalla algún objeto.*/
+/**
+ * Pintar por pantalla algún objeto.
+ * 
+ * @autor Lucas Bermudo Junco
+ */
 
 public class ObjetoSinColores{
   public static void main(String args[]){
     
-    char square = (char)9632, barraInvertida = (char)92, elipse = (char)11055;
+    char square = (char)9632, barraInvertida = (char)92;
 
     System.out.println("                 "+square+square+square+square+square+square+square+square+square+square+square+square+square+square+square);
     System.out.println("                //    @ │    @ ││");
