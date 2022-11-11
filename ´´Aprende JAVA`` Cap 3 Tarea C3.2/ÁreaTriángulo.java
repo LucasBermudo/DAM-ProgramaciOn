cdbb171f01bd88b130670f9451a7e555ab9e5239
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-11 03:30:31 +0100

    Update ÁreaTriángulo.java

--- ´´Aprende JAVA`` Cap 3 Tarea C3.2/ÁreaTriángulo.java
+++ ´´Aprende JAVA`` Cap 3 Tarea C3.2/ÁreaTriángulo.java
@@ -5,8 +5,8 @@
  */
 
 public class ÁreaTriángulo{
-  public static void main(String args[]){
-
+	public static void main(String args[]){
+	
 		String línea;
 		
 		System.out.print("Por favor, introduce la base del triángulo: ");
@@ -20,5 +20,5 @@ public class ÁreaTriángulo{
 		altura = Double.parseDouble(línea);
 		
 		System.out.println("El área de un rectángulo de base " + base + " y de altura " + altura + " unidades es " + ((base*altura)/2) + " unidades^2");
-  }
+	}
 }
