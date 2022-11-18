32684b545600be38ca80fde737f7cb3d8bd7b8f5
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-11-18 07:01:28 +0100

    Update BuenosDíasTardesNoches.java

--- ´´Aprende JAVA`` Cap 4 Tarea C4.1 - 2 & 4 al 5/BuenosDíasTardesNoches.java
+++ ´´Aprende JAVA`` Cap 4 Tarea C4.1 - 2 & 4 al 5/BuenosDíasTardesNoches.java
@@ -13,16 +13,16 @@ public class BuenosDíasTardesNoches{
 		
 		if(hora >= 0 && hora <= 23){
 			if(hora >= 6 && hora <= 12){
-			System.out.println("Buenos días, ¡que tengas un buen día!");
+				System.out.println("Buenos días, ¡que tengas un buen día!");
 			} else if(hora >= 13 && hora <= 20){
-			System.out.println("Buenas tardes, ¡que tengas una buena tarde!");
+				System.out.println("Buenas tardes, ¡que tengas una buena tarde!");
 			} else {
-			System.out.println("Buenas noches, ¡que tengas un buen descanso!");
+				System.out.println("Buenas noches, ¡que tengas un buen descanso!");
 			}		
 		} else if(hora == 24){
-		System.out.println("Lo sentimos, pero la hora introducida (" + hora + ") no existe. Puede que te estuvieras refiriendo a las 12 de la noche, en cuyo caso, te deseo buenas noches, ¡que tengas un buen descanso!");
+			System.out.println("Lo sentimos, pero la hora introducida (" + hora + ") no existe. Puede que te estuvieras refiriendo a las 12 de la noche, en cuyo caso, te deseo buenas noches, ¡que tengas un buen descanso!");
 		} else {
-		System.out.println("Lo sentimos, pero la hora introducida no existe.");
+			System.out.println("Lo sentimos, pero la hora introducida no existe.");
 		}
 	}
 }
