48ab1b27d2598b162d1f497528f2143876578339
LucasBermudo <117372664+LucasBermudo@users.noreply.github.com>
2022-12-21 14:10:06 +0100

    Update NúmeroAlRevés.java

--- ´´Aprende JAVA`` Cap 5 Tarea C5.4 - 21, 23, 15, 31 al 34, 37, 39 & 41/NúmeroAlRevés.java
+++ ´´Aprende JAVA`` Cap 5 Tarea C5.4 - 21, 23, 15, 31 al 34, 37, 39 & 41/NúmeroAlRevés.java
@@ -18,9 +18,9 @@ public class NúmeroAlRevés{
 		numMani = numOrig;					// Copiamos el número para no perderlo y así poderlo usar luego
 		
 		while(numMani>0){
-			resto = numMani%10;										// Extraemos la última cifra
-			numInv = numInv*10+resto;					// Desplazamos las cifras anteriores a la izquierda y extraemos la siguiente cifra, añadiéndola a la derecha
-			numMani/=10;															// Eliminamos la útlima cifra usada para poder acceder a la siguiente
+			resto = numMani%10;						// Extraemos la última cifra
+			numInv = numInv*10+resto;					// Desplazamos las cifras anteriores a la izquierda y añadimos la siguiente cifra a la derecha
+			numMani/=10;							// Eliminamos la útlima cifra usada para poder acceder a la siguiente después
 		}
 		
 		System.out.println("\nEl número " + numOrig + " al revés es " + numInv);
